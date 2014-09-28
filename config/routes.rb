bea550d88e9f7631f52a183a9a81e91e04b72bfd
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-09-28 18:51:20 +0100

    adds articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles
+
   get 'pages/home2'
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140926164906) do
+ActiveRecord::Schema.define(version: 20140928172138) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "opener"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
