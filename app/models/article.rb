a0a2961610465b31d243e7de09bef22f1b6a25eb
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-09-28 20:08:09 +0100

    formats article form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,13 @@
 class Article < ActiveRecord::Base
   has_attached_file :image, :styles =>  { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, 
+                              :default_url => "default.jpg", 
+                              :storage => :dropbox,
+                              :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                              :path => ":style/:id_:filename"
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+
  end
