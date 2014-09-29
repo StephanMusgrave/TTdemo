c2c15f227b7308115ee40ae0c3cd413feb94d748
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-09-29 20:19:26 +0100

    setting up articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,17 @@
 class Article < ActiveRecord::Base
-  has_attached_file :image, :styles =>  { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  if Rails.env.development?
+    has_attached_file :image, :styles =>  { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  else
+    has_attached_file :image, :styles =>  { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, 
-                              :default_url => "default.jpg", 
-                              :storage => :dropbox,
-                              :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                              :path => ":style/:id_:filename"
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, 
+                                :default_url => "default.jpg", 
+                                :storage => :dropbox,
+                                :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                                :path => ":style/:id_:filename"
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  end
 
-
- end
+end
